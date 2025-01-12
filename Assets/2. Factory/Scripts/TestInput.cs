1139f1fb27828153276d8cd9563e065e3bb6a9f5
dhghgh189 <dhghgh189@gmail.com>
2025-01-12 20:40:40 +0900

    [Fix] 아이템 생성 위치 수정

--- Assets/2. Factory/Scripts/TestInput.cs
+++ Assets/2. Factory/Scripts/TestInput.cs
@@ -8,12 +8,12 @@ public class TestInput : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            ItemManager.Instance.CreateItem("빨간 포션", new Vector3(-1, 1, 0));
+            ItemManager.Instance.CreateItem("빨간 포션", new Vector3(-1, 3, 0));
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            ItemManager.Instance.CreateItem("파란 포션", new Vector3(1, 1, 0));
+            ItemManager.Instance.CreateItem("파란 포션", new Vector3(1, 3, 0));
         }
     }
 }
