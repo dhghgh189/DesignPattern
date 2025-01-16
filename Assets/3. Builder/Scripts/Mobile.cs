e93d1583df338d1d941aa649274bb09b80c8bca0
dhghgh189 <dhghgh189@gmail.com>
2025-01-17 00:59:55 +0900

    [Feat] Mobile 생성자 추가
    - 외부에서 값을 넣어줄 수 있도록 생성자 추가

--- Assets/3. Builder/Scripts/Mobile.cs
+++ Assets/3. Builder/Scripts/Mobile.cs
@@ -7,16 +7,25 @@ public class Mobile
     private string productName;     // 제품명
     private string companyName;     // 회사명
     private float battery;          // 배터리 양
-    private string Resolution;      // 해상도
+    private string resolution;      // 해상도
     private string os;              // 운영체제
                                     // etc...
+
+    public Mobile(string productName, string companyName, float battery, string resolution, string os)
+    {
+        this.productName = productName;
+        this.companyName = companyName;
+        this.battery = battery;
+        this.resolution = resolution;
+        this.os = os;
+    }
 
     public void PrintSpec()
     {
         Debug.Log($"제품명 : {productName}");
         Debug.Log($"회사명 : {companyName}");
         Debug.Log($"배터리 양 : {battery}");
-        Debug.Log($"해상도 : {Resolution}");
+        Debug.Log($"해상도 : {resolution}");
         Debug.Log($"운영체제 : {os}");
     }
 }
